fdbedbf06a9520f1bd68ca8e908ade009c8999ef
Chingis Choybsonov <avatarius93@gmail.com>
2024-04-06 18:31:48 +0300

    начало реализации reset

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,14 +1,34 @@
 import { Gesture } from "./type";
 
-function chooseBotGesture(gestureArray: Gesture[]) {
+function getPlayerGesture(button: HTMLButtonElement): Gesture {
+  return button.dataset.gesture as Gesture;
+}
+
+function getBotGesture(gestureArray: Gesture[]) {
   const item = gestureArray[Math.floor(Math.random() * gestureArray.length)];
   return item;
 
 }
 
-function compareGestures() {
+function compareGestures(playerGesture: Gesture, botGesture: Gesture): number {
+  const gestureObj = {
+    'rock': {weakTo: 'paper', strongTo: 'scissors'},
+    'paper': {weakTo: 'scissors', strongTo: 'rock'},
+    'scissors': {weakTo: 'rock', strongTo: 'paper'}
+  };
+
+
+  if (playerGesture === gestureObj[botGesture].weakTo) {
+    return 1;
+  }
+
+  if (playerGesture === gestureObj[botGesture].strongTo) {
+    return -1;
+  }
+
+  return 0;
 
 }
 
 
-export {chooseBotGesture, compareGestures};
+export {getPlayerGesture, getBotGesture, compareGestures};
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,6 @@
 import "../pages/index.scss";
 import { Gesture } from "./type";
-import { chooseBotGesture } from "./game";
+import { getPlayerGesture, getBotGesture, compareGestures } from "./game";
 import { gestureArray } from "./constants";
 import paper from "../images/paper.svg";
 import rock from "../images/rock.svg";
@@ -8,6 +8,8 @@ import scissors from "../images/scissors.svg";
 
 const startContainer = document.querySelector<HTMLDivElement>(".start-screen")!;
 const board = document.querySelector<HTMLDivElement>(".board")!;
+const boardContainer = document.querySelector<HTMLDivElement>('.board__container')!;
+const boardText = document.querySelector<HTMLParagraphElement>('.board__text')!;
 const botContainer = document.querySelector<HTMLDivElement>(".player_right")!;
 const playerIcon = document.querySelector<HTMLDivElement>(".player__icon")!;
 const playerUse = document.querySelector<SVGUseElement>(
@@ -21,6 +23,7 @@ const botSvg = document.querySelector<SVGElement>(".circle-icon__svg_bot")!;
 const botUse = document.querySelector<SVGUseElement>(
   ".circle-icon__use_right"
 )!;
+const buttonRestart = document.querySelector<HTMLButtonElement>('.board__restart')!;
 
 function movePlayerIcon(
   movableElement: HTMLElement,
@@ -47,7 +50,12 @@ function changeIcon(gesture: Gesture, useElement: SVGUseElement) {
       url = "";
       break;
   }
+
   useElement.setAttribute("href", url);
+}
+
+function clearIcon(useElement: SVGUseElement) {
+  useElement.setAttribute("href", '');
 }
 
 function showBoardElements() {
@@ -79,14 +87,41 @@ function countdown(ms: number) {
   });
 }
 
-function toggleScreens(firstElem: HTMLElement, secondElem: HTMLElement): void {
-  if (firstElem.classList.contains("hidden")) {
-    firstElem.classList.remove("hidden");
-    secondElem.classList.add("hidden");
-  } else {
-    firstElem.classList.add("hidden");
-    secondElem.classList.remove("hidden");
+function showMessage(result: number, element: HTMLParagraphElement): void {
+  switch(result) {
+    case 0:
+      element.textContent = 'draw';
+      break;
+    case 1:
+      element.textContent = 'You win';
+      break;
+    case -1:
+      element.textContent = "You lose";
+      break;
   }
+}
+
+function toggleScreens(firstElem: HTMLElement, secondElem: HTMLElement): void {
+  const firstClassName = firstElem.classList[0];
+  const firstHiddenClassName = `${firstClassName}_hidden`;
+  const secondClassName = secondElem.classList[0];
+  const secondHiddenClassName=  `${secondClassName}_hidden`;
+
+
+  if (firstElem.classList.contains(firstHiddenClassName)) {
+    firstElem.classList.remove(firstHiddenClassName);
+    secondElem.classList.add(secondHiddenClassName);
+  } else {
+    firstElem.classList.add(firstHiddenClassName);
+    secondElem.classList.remove(secondHiddenClassName);
+  }
+}
+
+function restart() {
+  console.log('restart');
+  clearIcon(playerUse);
+  clearIcon(botUse);
+
 }
 
 startContainer.addEventListener("click", (evt: MouseEvent) => {
@@ -97,12 +132,14 @@ startContainer.addEventListener("click", (evt: MouseEvent) => {
   ) {
     return;
   }
+  let playerGesture: Gesture;
+  let botGesture: Gesture;
   // игрок выбирает жест
+  playerGesture = getPlayerGesture(target);
   toggleScreens(startContainer, board);
   movePlayerIcon(playerIcon, target);
 
-  const gesture = target.dataset.gesture as Gesture;
-  changeIcon(gesture, playerUse);
+  changeIcon(playerGesture, playerUse);
   // отобразить элементы board через 0.1 секунды
   showBoardElements()
     .then(() => {
@@ -110,10 +147,16 @@ startContainer.addEventListener("click", (evt: MouseEvent) => {
     })
     .then(() => {
       // бот выбирает жест
-      const choice: Gesture = chooseBotGesture(gestureArray);
+      botGesture = getBotGesture(gestureArray);
       botCounter.classList.add("player__counter_hidden");
       botSvg.classList.remove("circle-icon__svg_hidden");
-      changeIcon(choice, botUse);
+      changeIcon(botGesture, botUse);
+      boardContainer.classList.add('board__container_expanded');
+      const result = compareGestures(playerGesture, botGesture);
+      showMessage(result, boardText);
 
     });
 });
+
+
+buttonRestart.addEventListener('click' , restart);
